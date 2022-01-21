44d3c6f71314d705dba727e5bf019a6aee11e9eb
Boris2611 <84404796+Boris2611@users.noreply.github.com>
2022-01-21 15:48:40 +0100

    Add files via upload

--- Simple Email & Password Generator.py
+++ Simple Email & Password Generator.py
@@ -1,16 +1,55 @@
 import random
 
 file = open("output.txt", "w+")
+names = open("Names.txt", "r")
 
 generating = int(input("Number of Emails & Passwords: "))
 
-for i in range(generating):
-    file.write("{0:10}".format(str(i + 1) + ") "))
-    
-    letters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+letters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
                 "a", "s", "d", "f", "g", "h", "j", "k", "l", "z",
                 "x", "c", "v", "b", "n", "m"]
 
+# Creating Random Name List
+namesList = []
+for i in range(500):
+    name = names.readline()
+    
+    if ("\n" in name):
+        name = name.replace ("\n", "")
+    
+    namesList.append(name)
+            
+random.shuffle(namesList)
+
+for i in range(generating):
+
+    
+    # Counter
+    file.write("{0:10}".format(str(i + 1) + ") "))
+
+
+    # ---------- EMAIL ----------------
+    email = ""
+
+    nameIndex = random.randint(0, 499)    
+
+    email += namesList[nameIndex]
+    email += str(random.randint(1989, 2021))
+    email += "@"
+    
+    emailEx = ["gmail", "yahoo", "outlook", "hotmail"]
+    index = random.randint(0,3)
+    email += emailEx[index]
+    
+    email += ".com"
+    
+    # Formating Email
+    file.write("{0:35}".format(email))
+    # ----------------------------------
+    
+
+
+    # ---- Password ---------
     passwordLong = []
     for i in range(25):
         passwordLong.append(letters[i])
@@ -22,50 +61,15 @@ for i in range(generating):
     random.shuffle(passwordLong)
 
 
-    emailLong = []
-    for i in range(25):
-        emailLong.append(letters[i])
-    for i in range(10):
-        emailLong.append(str(i))
-
-    emailLenght = random.randint(7, 15)
-    random.shuffle(emailLong)
-
-    file.write("Email:  ")
-    email = ""
-    
-    for i in range(emailLenght):
-        email += emailLong[i]
-    email += "@"
-
-    emailExtension = ["gmail", "yahoo", "outlook", "hotmail", "yandex.mail"]
-    
-
-    email += emailExtension[random.randint(0, 4)]
-    
-
-    email += ".com"
-    
-    file.write("{0:35}".format(email))
-
-    
-
-
-    
-
-
     file.write("Password:  ")
     for i in range(passwordLenght):
         file.write(passwordLong[i])
 
     file.write("\n")
-    
-
-
-
-
+    # -----------------------
 
 
 
 file.close()
+names.close()
 exit()
